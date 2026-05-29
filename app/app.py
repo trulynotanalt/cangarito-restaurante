0a005085beb92dc66e2c033590131cdbea4b9606
João_pedro <jp.pedrinho1204@gmail.com>
2026-05-29 11:23:01 -0300

    criação das sessões
    criei a sessão de usuario, após o login, e outra funcionalidades

--- app/app.py
+++ app/app.py
@@ -2,22 +2,21 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 
 app = Flask(__name__)
 
-
-
+app.secret_key = 'chave-super-secreta'
+usuarioLista= []
 
 @app.route('/')
 def landingpage():
     #boa sorte bd.py, vulgo joão victor noberto tomaz santana
-    # if 'roro' not in session:
-    #     return redirect(url_for('cadastro'))
+  
     return render_template('landing-page.html')
 
 @app.route('/cadastro', methods=['GET','POST'])
 def cadastro():
-
+    global usuarioLista
     #cara do bd.py q se vire aq
-    # if 'roro' in session:
-    #     return redirect(url_for('landingpage'))
+    if 'usuario' in session:
+        return redirect(url_for('landingpage'))
     
     if request.method == 'POST':
         #aq o cara do banco de dados vai usar essas infos para fazer a confirmação e integração com o bd.py
@@ -25,22 +24,35 @@ def cadastro():
         senha = request.form.get('senha')
         c_senha = request.form.get('c_senha')
 
+        usuarioLista.append(email,senha)
+        
+
+        
+
+        return redirect(url_for('login'))
 
 
-    
+  
     return render_template('cadastro.html')
 
 @app.route('/login', methods=['GET','POST'])
 def login():
 
     #é necessario o cara de bd.py ai, se n eu faço tudo sozinho e ele n quer isso
-    # if 'roro' in session:
-    #     return redirect(url_for('landingpage'))
+    if 'usuario' in session:
+        return redirect(url_for('landingpage'))
     
     if request.method == 'POST':
         #aq o cara do banco de dados vai usar essas infos para fazer a confirmação e integração com o bd.py
         email = request.form.get('email')
         senha = request.form.get('senha')
+                
+        if usuarioLista[0] != email or usuarioLista[1] != senha:
+            return redirect(url_for('login'))
+
+        session['usuario'] = email
+
+        
 
         
         #coisas para o cara do bd.py se preocupar em botar, n se esqueça do else
@@ -56,8 +68,8 @@ def login():
 def cardapio():
 
     #ia esquecendo esse
-    # if 'roro' not in session:
-    #     return redirect(url_for('cadastro'))
+    if 'usuario' not in session:
+        return redirect(url_for('cadastro'))
     
     return render_template('cardapio.html')
 
@@ -65,8 +77,8 @@ def cardapio():
 def carrinho():
 
     #aq tbm tem q mexer coisa linda
-    # if 'roro' not in session:
-    #     return redirect(url_for('cadastro'))
+    if 'usuario' not in session:
+        return redirect(url_for('cadastro'))
     
     return render_template('carrinho.html')
 
@@ -74,8 +86,8 @@ def carrinho():
 def happyhour():
 
     #acaba nunca, avemaria
-    # if 'roro' not in session:
-    #     return redirect(url_for('cadastro'))
+    if 'usuario' not in session:
+       return redirect(url_for('cadastro'))
     
     return render_template('happyhour.html')
 
@@ -84,8 +96,8 @@ def happyhour():
 def perfil():
 
     #ainda bem q acabou, eu acho
-    # if 'roro' not in session:
-    #     return redirect(url_for('cadastro'))
+    if 'usuario' not in session:
+        return redirect(url_for('cadastro'))
     
     return render_template('perfil.html')
 
